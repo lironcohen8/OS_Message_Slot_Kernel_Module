fe204910266c9d45fedc23c18f3ef7c3e237fe17
Liron Cohen <lironcohen3@mail.tau.ac.il>
2021-12-04 19:57:36 +0200

    deleted TODO

--- message_slot.h
+++ message_slot.h
@@ -9,13 +9,7 @@
 #define MAX_MESSAGE_LENGTH 128
 #define MAX_SLOTS_NUM 256
 
-// TODO understand if needed
 // Sets the file descriptor's channel id
 #define MSG_SLOT_CHANNEL _IOW(MAJOR_NUM, 0, unsigned long)
-
-// TODO delete if not needed
-//#define BUF_LEN 80
-//#define DEVICE_FILE_NAME "simple_char_dev"
-//#define SUCCESS 0
 
 #endif
